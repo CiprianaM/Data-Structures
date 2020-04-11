94e9da4a57b18742204ff60309f7ad39855bb803
CiprianaM <cipriana2001@gmail.com>
2020-04-11 18:03:26 +0200

    add tail and adapt methods

--- lists/singleLinkedList.js
+++ lists/singleLinkedList.js
@@ -1,82 +1,111 @@
+const {SingleNode} = require('./node');
 class SingleLinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
   }
-  //helper function
-  checkHead() {
-    return !!this.head;
-  }
-  //helper function
-  findPreviousNode(position) {
-    let currentNode = this.head;
-    let index = 0;
-    while (position>index+1) {
-      index++;//1
-      currentNode = currentNode.next;//A
-    }
-    return currentNode;
-  }
-  insertNodeAtHead(node) {
-    this.checkHead() ? (node.next = this.head) && (this.head = node) : this.head = node
-    return this.head;
-  }
-  insertNodeAtTail(node) {
-    if (!this.checkHead()) {
+  insertAtHead(value) {
+    const node = new SingleNode(value);
+    if (!this.head) {
+      this.head = this.tail = node;
+    } else {
+      node.next = this.head;
       this.head = node;
+    };
+    return true;
+  }
+  insertAtTail(value) {
+    const node = new SingleNode(value);
+    if(!this.head) {
+      this.head = this.tail = node;
     } else {
       let currentNode = this.head;
-      while (currentNode.next!==null) {
+      while (currentNode.next) {
         currentNode = currentNode.next;
       }
       currentNode.next = node;
+      this.tail = node;
     }
-    return this.head;
+    return true;
   }
-  insertNodeAtPosition(nodeToInsert, position) {
-    let currentNode = this.findPreviousNode(position);//B
-    nodeToInsert.next = currentNode.next;
-    currentNode.next = nodeToInsert;
-    return this.head;
+
+  insertAtPosition(value, position) {
+    const node = new SingleNode(value);
+    let index = 0;
+    let currentNode = this.head;
+    while(index+1<position && currentNode.next) {
+      index++;
+      currentNode=currentNode.next;
+    }
+    if (index+1<position) {console.log('could not insert'); return false}
+    node.next=currentNode.next;
+    currentNode.next=node;
+    return true;
   }
+
   deleteFromHead() {
-    if (!this.checkHead()) {
-      console.log('not possible')
-      return null;
+    if (this.head===null) {
+      return false;
+    }
+    if (this.head===this.tail) {
+      this.head = this.tail = null;
     } else {
-      console.log('head deleted')
       this.head = this.head.next;
     }
-    return this.head;
+    return true;
   }
   deleteFromTail() {
-    if (!this.checkHead()) {
-      return null;
+    if (this.head===null) {
+      return false;
+    }
+    if (this.head===this.tail) {
+      this.head = this.tail = null;
     } else {
       let currentNode = this.head;
-      if (currentNode.next===null) {
-        this.head = null;
-        return this.head;
-      } else {
-        while (currentNode.next.next!==null) {
+      if (currentNode.next) {
+        while (currentNode.next.next) {
           currentNode = currentNode.next;
         }
         currentNode.next = null;
-        return this.head;
+        this.tail = currentNode;
       }
     }
+    return true;
   }
   deleteFromPosition(position) {
-    let previousNode = this.findPreviousNode(position);
-    // console.log(previousNode)
-    if (previousNode.next===null || previousNode===undefined) {
-      // console.log('node doesnt exit', previousNode);
-      return null;
-    } else {
-      previousNode.next = previousNode.next.next;
-      return null;
+    let index = 0;
+    let currentNode = this.head;
+    while((index+1)<position &&currentNode.next) {
+      index++;
+      currentNode=currentNode.next
     }
-
+    if (currentNode.next) {
+      if (currentNode==this.head) {
+        this.head = currentNode.next;
+        return true;
+      }
+      currentNode.next=currentNode.next.next;
+      if (!currentNode.next) {this.tail=currentNode}
+      return true
+    } else {
+      if (!currentNode.next) {
+        console.log('position outside range')
+        return false;
+      }
+    }
+    return false;
   }
 }
+
+// let list = new SingleLinkedList();
+// list.insertAtHead('A');//A
+// list.insertAtTail('B');//AB
+// list.insertAtTail('C')//ABC
+// list.insertAtTail('D')//ABCD
+// list.insertAtPosition('E', 2)//ABECD
+// list.deleteFromHead()//BECD
+// list.deleteFromHead()//ECD
+// list.deleteFromPosition(0)//CD
+// console.log(list)
 
 module.exports = SingleLinkedList;
